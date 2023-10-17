b2752bff08bb30c301d482057c703cf4cba67e7b
ezgaro <ezgaro2005@gmail.com>
2023-10-17 14:26:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict"
+
+
+let secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    displayMessage("No number!");
+  }else if(guess === secretNumber){
+    displayMessage('🎉Correct Number!');
+    document.querySelector('body').style.backgroundColor = "#60b347";
+    document.querySelector('.number').style.width = "30rem";
+    document.querySelector('.number').textContent = secretNumber;
+    if(score>highscore){
+      highscore=score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }else if(guess!==secretNumber){
+    if(score>1){
+      displayMessage(guess>secretNumber?'📈Too high!':'📉Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+      }else{
+        displayMessage('🚫 You lost the game!');
+        document.querySelector('.score').textContent = 0;
+      }
+  }
+})
+document.querySelector('.again').addEventListener('click' , function(){
+  score=20;
+  secretNumber = Math.trunc(Math.random()*20) + 1;
+  document.querySelector('.score').textContent = score;
+  // document.querySelector('.highscore').textContent = highscore;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = "#222";
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = "15rem";
+})
